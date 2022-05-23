ee8848da7b714b6dddc19b071eede5422ebe1fed
Arahmna75 <selfaraman01825@gmail.com>
2022-05-24 01:11:43 +0600

    added hello api

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const database = client.db('carMechanic');
         const servicesCollection = database.collection('services');
-        
-//Get api
-app.get('/services', async(req, res)=>{
-    const cursor = servicesCollection.find({});
-    const services = await cursor.toArray();
-    res.send(services)
-});
-//Get Single Api
-app.get('/services/:id', async(req, res)=>{
-    const id = req.params.id;
-    // console.log('Getting specific service', id);
-    const query = {_id: ObjectId(id)};
-    const services = await servicesCollection.findOne(query);
-    res.json(services);
-})
+
+        //Get api
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+        //Get Single Api
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log('Getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const services = await servicesCollection.findOne(query);
+            res.json(services);
+        })
 
         //Post Api
-        app.post('/services', async(req, res)=>{
+        app.post('/services', async (req, res) => {
             const service = req.body;
-           console.log('hitting the post api', service);
+            console.log('hitting the post api', service);
             const result = await servicesCollection.insertOne(service);
             console.log(result);
 
@@ -48,17 +48,17 @@ app.get('/services/:id', async(req, res)=>{
 
         });
         //Delete service
-        app.delete('/services/:id', async(req, res)=>{
+        app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await servicesCollection.deleteOne(query);
             res.json(result)
         })
-        
+
     }
-finally{
-    // await client.close();
-}
+    finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -66,6 +66,32 @@ app.get('/', (req, res) => {
     res.send('Running genius server!');
 });
 
+app.get('/hello', (req, res)=>{
+    res.send('hello updated here')
+})
+
 app.listen(port, () => {
     console.log('Running genius server on port', port);
 });
+
+/*
+One time 
+1.heroku account open
+2. Heroku software install
+
+every project 
+1.git init
+2..gitignore (node_module, .env)
+3.push everything to git
+4.make sure you have this script: "node index.js"
+5.make sure: put process.env.PORT in front your port number
+6.Heroku login
+7.Heroku create (only one time for a project)
+8. command: git push heroku main
+
+---------
+update:
+1.Save everything check locally
+2. git add, git commit -m"ji", git push
+3.git push heroku main
+*/
